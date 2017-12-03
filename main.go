cb0c326c74aa236a5d027561aa66d2da4925f6b8
Kat <kittyhacker101@protonmail.com>
2017-12-03 15:57:18 -0800

    Proxy bug fixes

--- main.go
+++ main.go
@@ -59,9 +59,11 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 }
 
 func director(r *http.Request) {
+	u := r.URL.EscapedPath()
+
 	/* This never returns an error for some reason, so why bother handling it?
 	Not to mention, there's not any real way to handle an error here anyways. */
-	r.URL, _ = url.Parse(conf.Proxy.URL)
+	r.URL, _ = url.Parse(conf.Proxy.URL + strings.TrimPrefix(u, "/"+conf.Proxy.Loc))
 }
 
 var (
