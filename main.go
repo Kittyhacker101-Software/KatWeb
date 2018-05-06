6b51983f8696be749343556fce8e096034f7d3ff
Kat <kittyhacker101@protonmail.com>
2018-05-05 17:16:18 -0700

    Minor changes
    It turns out you can just include the normal cipher suites, and still take advantage of TLS 1.3

--- main.go
+++ main.go
@@ -69,12 +69,7 @@ var (
 		},
 		MinVersion: tls.VersionTLS12,
 		CipherSuites: []uint16{
-			// TLS 1.3+ only, requires tls-tris
-			//tls.TLS_CHACHA20_POLY1305_SHA256,
-			//tls.TLS_AES_256_GCM_SHA384,
-			//tls.TLS_AES_128_GCM_SHA256,
-
-			// TLS 1.2
+			// NOTE: Please compile with TLS-Tris if you would like to take advantage of TLS 1.3
 			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
