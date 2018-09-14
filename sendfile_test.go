42894e1dfab669da39ed641dc332592a70db64da
Kat <kittyhacker101@protonmail.com>
2018-09-14 14:57:16 -0700

    Added more unit tests

--- sendfile_test.go
+++ sendfile_test.go
@@ -2,9 +2,10 @@
 package main
 
 import (
-	//"net/http"
-	//"net/http/httptest"
-	//"runtime/debug"
+	"net/http"
+	"net/http/httptest"
+	"bytes"
+	"strings"
 	"os"
 	"testing"
 )
@@ -76,4 +77,56 @@ func Test_isZipped(t *testing.T) {
 	}
 
 	os.Remove("html/test.txt")
+}
+
+func Test_StyledError(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		StyledError(w, "Hi", "Hello KatWeb!", 200)
+	}))
+	resp, err := server.Client().Get(server.URL)
+	if err != nil {
+		t.Fatal("Unable to get testing data!")
+	}
+
+	if resp.StatusCode != 200 || resp.Header.Get("Content-Type") != "text/html; charset=utf-8" {
+		t.Fatal("StyledError is not functioning correctly!")
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if !strings.Contains(buf.String(), "Hi") || !strings.Contains(buf.String(), "Hello KatWeb!") {
+		t.Fatal("StyledError is not functioning correctly!")
+	}
+
+	resp.Body.Close()
+}
+
+func Test_dirList(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file, err := os.Open("html")
+		if err != nil {
+			http.Error(w, "Unable to read directory" ,500)
+			return
+		}
+		if dirList(w, *file, "localTest/html") != nil {
+			http.Error(w, "Unable to crawl directory" ,500)
+		}
+	}))
+	resp, err := server.Client().Get(server.URL)
+	if err != nil {
+		t.Fatal("Unable to get testing data!")
+	}
+
+	if resp.StatusCode != 200 || resp.Header.Get("Content-Type") != "text/html; charset=utf-8" {
+		t.Fatal("DirList is not functioning correctly!")
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if !strings.Contains(buf.String(), "localTest/html") || !strings.Contains(buf.String(), "index.html") || !strings.Contains(buf.String(), "DemoPass") || !strings.Contains(buf.String(), "special ^&#34;&#39;.test") {
+		Print(buf.String())
+		t.Fatal("DirList is not functioning correctly!")
+	}
+
+	resp.Body.Close()
 }
